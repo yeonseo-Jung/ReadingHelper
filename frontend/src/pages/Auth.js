643079b9f29e0b8bf5b03eb54a5e221265175ee6
pySoo <55135881+pySoo@users.noreply.github.com>
2021-07-19 05:44:44 +0900

    Initial Setting

---
+++ frontend/src/pages/Auth.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Grid } from '@material-ui/core';
+
+function Home() {
+	return <Grid className="app">Auth</Grid>;
+}
+
+export default Home;
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Grid } from '@material-ui/core';
+
+function Home() {
+	return <Grid className="app">Home</Grid>;
+}
+
+export default Home;
