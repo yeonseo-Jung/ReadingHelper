4cc4e49e6e562a62096ade3053e9339a30b60969
pySoo <55135881+pySoo@users.noreply.github.com>
2021-07-19 09:20:52 +0900

    Change Header UI

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,20 +1,66 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Grid, Button } from '@material-ui/core';
+import TitleImg from './styles/title_icon.jpg';
 
 const Header = () => {
 	return (
-		<Grid className="header">
-			<Link to="/">
-				<Button className="header-icon">독서 비서</Button>
-			</Link>
+		<Grid container className="header">
+			<Grid className="header-title">
+				<Link to="/">
+					<Button>
+						<Grid className="header-img">
+							<img
+								src={TitleImg}
+								width="50"
+								height="50"
+								alt="title"
+							/>
+						</Grid>
+						<Grid container direction="row" className="header-icon">
+							<Grid className="title-y">독</Grid>
+							<Grid className="title-g">서</Grid>
+							<Grid className="title-y">비</Grid>
+							<Grid className="title-g">서</Grid>
+						</Grid>
+					</Button>
+				</Link>
+			</Grid>
+			<Grid className="header-menu" xs={6}>
+				<Link to="/mylib">
+					<Button>내 서재</Button>
+				</Link>
+				<Grid className="menu-line" />
+				<Link to="/report">
+					<Button>회원가입</Button>
+				</Link>
+				<Grid className="menu-line" />
+				<Link to="/calendar">
+					<Button>독서 달력</Button>
+				</Link>
+			</Grid>
 			<Grid className="header-buttons">
-				<Grid>사용자님, 환영합니다!</Grid>
 				<Link to="/login">
-					<Button>로그인</Button>
+					<Button
+						style={{
+							borderRadius: 20,
+							backgroundColor: '#fdd45a',
+						}}
+						variant="contained"
+					>
+						로그인
+					</Button>
 				</Link>
 				<Link to="join">
-					<Button>회원가입</Button>
+					<Button
+						style={{
+							borderRadius: 20,
+							backgroundColor: '#fdd45a',
+						}}
+						variant="contained"
+					>
+						회원가입
+					</Button>
 				</Link>
 			</Grid>
 		</Grid>
