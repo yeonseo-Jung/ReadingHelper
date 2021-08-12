1df15de127e7c1f859cf8ff3f1cd982af1aeeaf0
pySoo <55135881+pySoo@users.noreply.github.com>
2021-08-12 18:45:10 +0900

    Kakao login: Get code from fe and hand over userinfo

--- backend/src/main/java/com/forweber/backend/controller/UserController.java
+++ backend/src/main/java/com/forweber/backend/controller/UserController.java
@@ -1,19 +1,50 @@
 package com.forweber.backend.controller;
 
+import com.forweber.backend.domain.AuthKakao;
+import com.forweber.backend.service.AuthKakaoService;
+import com.forweber.backend.service.AuthNaverService;
 import com.forweber.backend.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class UserController {
+    @Autowired
     private final UserService userService;
+    @Autowired
+    private AuthKakaoService kakaoService;
+    @Autowired
+    private AuthNaverService naverService;
 
     // Redirect URL을 통해 프론트로부터 인자 코드를 받음
     @GetMapping("/login/callback/kakao")
-    public void oauthKakao(@RequestParam("code") String code) throws Exception {
-        userService.oauthKakao(code);
+    public HashMap<String, Object> loginKakao(@RequestParam("code") String code) throws Exception {
+        AuthKakao authorization = kakaoService.getAccessToken(code);
+        System.out.println("controller access_token : " + authorization);
+
+        HashMap<String, Object> userInfo = kakaoService.getUserInfo(authorization.getAccess_token());
+        userInfo.put("access_token", authorization.getAccess_token());
+        userInfo.put("refresh_token", authorization.getRefresh_token());
+        return userInfo;
+    }
+
+    @RequestMapping("/login/callback/naver")
+    public void loginNaver(@RequestParam("code") String code) throws Exception {
+        //userService.oauthNaver(code, request, response);
+//        log.info("log", code, request, session);
+
+
     }
 }
--- backend/src/main/java/com/forweber/backend/service/UserService.java
+++ backend/src/main/java/com/forweber/backend/service/UserService.java
@@ -1,18 +1,6 @@
 package com.forweber.backend.service;
-
-import com.forweber.backend.domain.AuthKakao;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class UserService {
-    private final AuthKakaoService authKakaoService;
-
-    // 카카오로에 인자 코드를 넘겨서 token을 받음, token으로 유저 정보를 얻을 수 있음
-    public void oauthKakao(String code) throws Exception {
-        AuthKakao authorization = authKakaoService.callTokenApi(code);
-        String userInfoFromKakao = authKakaoService.callGetUserByAccessToken(authorization.getAccess_token());
-        System.out.println("userInfoFromKakao = " + userInfoFromKakao);
-    }
 }
